ccb5008df0d3c6777f94738c4b0c6ecfd84c0db5
liuhuacong <Liuhuacong8>
2022-10-17 16:35:53 +0800

    添加分组

--- internal/controller/system/group/group.go
+++ internal/controller/system/group/group.go
@@ -1,14 +1,44 @@
 package group
 
 import (
+	"game.sdk.center/internal/model/common"
 	"game.sdk.center/internal/model/system"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func Add(c *gin.Context) {
 	group := system.NewGroup()
-
+	result := common.NewResult(c)
 	if err := c.ShouldBind(&group); err != nil {
+		result.Fail("参数格式错误，请检查")
+		log.WithField("request_id", c.GetString("request_id")).Error(err)
 		return
 	}
+
+	if err := group.Add(); err != nil {
+		result.Error(err)
+		log.WithField("request_id", c.GetString("request_id")).Error(err)
+		return
+	}
+
+	result.Success()
+}
+
+func Update(c *gin.Context) {
+	group := system.NewGroup()
+	result := common.NewResult(c)
+	if err := c.ShouldBind(&group); err != nil {
+		result.Fail("参数格式错误，请检查")
+		log.WithField("request_id", c.GetString("request_id")).Error(err)
+		return
+	}
+
+	if err := group.Update(); err != nil {
+		result.Error(err)
+		log.WithField("request_id", c.GetString("request_id")).Error(err)
+		return
+	}
+
+	result.Success()
 }
--- internal/model/system/group.go
+++ internal/model/system/group.go
@@ -2,28 +2,53 @@ package system
 
 import (
 	"errors"
+	"game.sdk.center/internal/model/common"
 	"game.sdk.center/tool"
-	"gorm.io/gorm"
 )
 
 type Group struct {
-	gorm.Model
-	Name string `gorm:"name"`
+	common.Model
+	Name string `gorm:"name" form:"name"`
+	Flag string `gorm:"flag" form:"flag"`
 }
 
 func NewGroup() *Group {
 	return &Group{}
 }
 
-func (this *Group) Add() error {
-	if this.Name == "" {
+func (g *Group) Add() error {
+	if g.Name == "" {
 		return errors.New("名称不能为空")
 	}
+	if g.Flag == "" {
+		return errors.New("标识不能为空不能为空")
+	}
 
-	if err := tool.MysqlDb.Model(this).Create(this).Error; err != nil {
+	if err := tool.MysqlDb.Model(g).Where("name", g.Name).Take(&Group{}).Error; err == nil {
+		return errors.New("该名称已存在")
+	}
+
+	if err := tool.MysqlDb.Model(g).Where("flag", g.Flag).Take(&Group{}).Error; err == nil {
+		return errors.New("该标识已存在")
+	}
+
+	if err := tool.MysqlDb.Model(g).Create(g).Error; err != nil {
 		return err
 	}
 
 	return nil
 
+}
+
+func (g *Group) Update() error {
+
+	if g.ID == 0 {
+		return errors.New("id 无效")
+	}
+
+	if err := tool.MysqlDb.Model(g).Where("id", g.ID).Updates(g).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
