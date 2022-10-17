4ea430d8c72a1c35e44f150eb7dc4a4a9d21a682
liuhuacong <Liuhuacong8>
2022-10-17 16:58:17 +0800

    公共响应

--- internal/middleware/request.go
+++ internal/middleware/request.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
-func RequestId() gin.HandlerFunc {
-	return func(cxt *gin.Context) {
+func RequestId(cxt *gin.Context) {
+	fmt.Println("------------------------")
+	cxt.Next()
 
-	}
+	fmt.Println("*******************************")
 }
